4d0fe5184d950304de88587c8212144cc0003f62
malak akkas <akkas.malak@gmail.com>
2021-07-11 16:15:25 +0300

    First day assighnments

---
+++ Hoist.js
@@ -0,0 +1,112 @@
+// console.log(hello);
+// var hello = 'world';
+
+// var hello;
+// console.log(hello);
+// hello = 'world';
+
+//2
+
+
+// var needle = 'haystack';
+// test();
+
+// function test() {
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+
+// var needle;
+// test();
+
+// function test() {
+//     var needle;
+//     console.log(needle);
+//     needle = 'magnet';
+// }
+// needle = 'haystack';
+
+//output ="magnet"
+
+//3
+
+// var brendan;
+// brendan = 'super cool';
+
+// function print() {
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+//output = super cool
+
+
+
+//4
+//interpreter-code
+
+// var food;
+// food = 'chicken';
+// console.log(food);
+// eat();
+
+// function eat() {
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+//     food = 'gone';
+// }
+//output =chicken
+//half-chicken
+
+
+//5
+// mean();
+// console.log(food);
+
+// var mean;
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food;
+//     food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+// output = ERROR
+
+//6
+
+// console.log(genre);
+// var genre;
+// genre = "disco";
+// rewind();
+
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre;
+//     genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+// output=
+// undefined
+// rock
+// r & b
+// disco
+
+//7
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo;
+//     dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);   //output = san jose, seattle, burbank, san jose
