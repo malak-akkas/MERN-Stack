d3847002c9174a2c47d17aead72c9035a5cf7fcc
malak akkas <akkas.malak@gmail.com>
2021-07-13 14:53:26 +0300

    First React App

---
+++ React/myapp/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App" >
+            <h1> Hello Dojo! </h1>
+         <h4> Things I need to do:</h4>
+         <div>
+             <ul>
+                 <li>Learn React</li>
+                 <li>Sleep</li>
+                 <li>Sleep</li>
+                 <li>Sleep</li>
+
+
+
+
+             </ul>
+         </div>
+        </div>
+    );
+}
+
+export default App;
